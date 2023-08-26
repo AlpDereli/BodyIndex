926643238b2013dfe1e5d972a1ce42e3be60f69c
AlpDereli <alpdereli26@gmail.com>
2023-08-26 15:09:14 +0300

    code added

--- src/BodyIndex.java
+++ src/BodyIndex.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BodyIndex {
+import java.util.Scanner;
+public class BodyIndex {
+    public static void main(String[] args) {
+        Scanner take = new Scanner(System.in);
+        System.out.println("Please enter your height as meters: ");
+        float h = take.nextFloat();
+        System.out.println("Please enter your weight as KG: ");
+        float w = take.nextFloat();
+        float f = w /(h*h);
+        System.out.println("Your index: " + f);
+    }
 }
